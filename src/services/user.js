f89df68645a3f56dde03a308724a3e84f26563ab
李清振 <develop.universal@gmail.com>
2018-01-22 17:40:45 +0800

    测试提交

--- src/services/user.js
+++ src/services/user.js
@@ -7,3 +7,10 @@ export async function query() {
 export async function queryCurrent() {
   return request('/api/currentUser');
 }
+
+export async function accountLogin(params) { // 用户登录
+  return request(`${global.API_PREFIX}/mobile/login`, {
+    method: 'POST',
+    body: params,
+  });
+}
